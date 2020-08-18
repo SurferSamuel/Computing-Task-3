66ecfd330dcfd4acc050a36febbcb4e90c77cba8
Samuel J. Mac <johnstonsamuel@inaburrastudents.nsw.edu.au>
2020-08-18 19:43:31 +1000

    Update 1.1

---
+++ COVID-19 Simulation/Assets/InfectantTransmission.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfectantTransmission : MonoBehaviour
+{	
+	public Transform gameObjects;
+	
+	private Transform circles;
+	
+	SpriteRenderer spriteRenderer;
+
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // Assign components in circles
+		circles = gameObjects.GetComponentInChildren<Transform>();
+		
+		foreach (Transform child in circles)
+		{
+			var distance = Vector2.Distance(child.transform.position, gameObject.transform.position);
+			
+			if (distance != 0 && distance < 1.5)
+			{
+				spriteRenderer.color = new Color (255, 0, 0, 255);
+			}
+			if (distance >= 1.5)
+			{
+				spriteRenderer.color = new Color (255, 255, 255, 255);
+			}
+		}
+    }
+}
---
+++ COVID-19 Simulation/Assets/Scripts/GameObjectMovement.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjectMovement : MonoBehaviour
+{
+	private Rigidbody2D rb;
+	
+	private bool wander_enabled;
+	
+	private float wander_speed;
+	private float wander_direction;
+	
+	// Start is called before the first frame update
+    void Start()
+    {	
+		// Assign rigidbody2D
+		rb = GetComponent<Rigidbody2D>();
+		
+		// Assign wander to be enabled
+		wander_enabled = true;
+		
+		// Start wandering
+		StartCoroutine(Wander());
+
+    }
+	
+	IEnumerator Wander()
+	{
+		// Pick a random direction on start
+		transform.Rotate(new Vector3(0, 0, Random.Range(0f, 360f)));	
+		
+		// Pick a random speed on start
+		wander_speed = Random.Range(0.80f, 1.20f);
+		
+		// Start initial movement
+		rb.AddRelativeForce(transform.up * wander_speed, ForceMode2D.Impulse);
+		
+		while(wander_enabled)
+		{
+			// Reset velocity to 0
+			rb.velocity = Vector2.zero;
+			
+			// Reset angular velocity to 0
+			rb.angularVelocity = 0f;
+			
+			// Reset direction
+			//transform.Rotate(new Vector3(0f, 0f, 0f));
+			
+			// Pick random direction
+			wander_direction = Random.Range(0f, 360f);
+			
+			// Apply direction change
+			transform.Rotate(new Vector3(0f, 0f, wander_direction));
+			
+			// Add force for movement
+			rb.AddForce(transform.up * wander_speed, ForceMode2D.Impulse);
+
+			// Wait for 0.5 seconds
+			yield return new WaitForSeconds(2f);
+		}
+	}
+}

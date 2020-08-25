53d4e525d3b1ee9c0cdc8b03a8ba7b7a8202689e
Samuel J. Mac <johnstonsamuel@inaburrastudents.nsw.edu.au>
2020-08-25 18:15:32 +1000

    Update 1.42

---
+++ COVID-19 Simulation/Assets/TransmissionValueController.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransmissionValueController : MonoBehaviour
+{
+    [Range(0, 100)]
+	public int TransmissionChance;
+	
+}

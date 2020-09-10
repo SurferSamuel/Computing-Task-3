3cfdd558e0f8d299a140dc882f407d22e2db3d13
Samuel J. Mac <johnstonsamuel@inaburrastudents.nsw.edu.au>
2020-09-10 14:54:02 +1000

    Update 1.52

--- COVID-19 Simulation/Assets/DataExporter.cs
+++ COVID-19 Simulation/Assets/DataExporter.cs
@@ -2,14 +2,58 @@
 using System; 
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks
-using Microsoft.Office.Interop.Excel;
-using _Excel = Microsoft.Office.Interop.Excel;
+using System.Threading.Tasks;
 	
 public class DataExporter : MonoBehaviour
 {
-	string path = "";
 	
+	private string pathOriginName = "SimulationResults";
+	public string filepath = "";
+	
+	private int testNum = 0;
+	
+	void Start()
+	{
+		// While loop to check for files
+		while (true)
+		{	
+			// If file with name already exists
+			if (File.Exists(pathOriginName + testNum + ".csv"))
+			{
+				// Add one to the num count
+				testNum += 1;
+			}
+			else
+			{	
+				// If file doesn't exists, use this pathName
+				filepath = pathOriginName + testNum + ".csv";
+				
+				// End while loop
+				break;
+			}
+		}
+	}
+	
+	public void addRecord(string timeStamp, string numNotInfected, string numInfected, string numDead)
+	{
+		try
+		{
+			// Open to new line in file
+			using (System.IO.StreamWriter file = new System.IO.StreamWriter(@filepath, true))
+			{
+				// Write in line
+				file.WriteLine(timeStamp + "," + numNotInfected + "," + numInfected + "," + numDead);
+			}
+		}
+		
+		// If try returns an error
+		catch(Exception ex)
+		{
+			// Show error in console
+			Debug.Log("Error in DataExporter: " + ex);
+		}
+	}
 }
---
+++ COVID-19 Simulation/Assets/PopulationChecker.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopulationChecker : MonoBehaviour
+{
+    public bool checkerEnabled = true;
+   
+   	public int infectedCount;
+	public int nonInfectedCount;
+	public int deadCount;
+	public int recoveredCount;
+	
+	private Component[] circleChildren;
+	
+    void Start()
+	{
+		// Start circleChecker coroutine
+		StartCoroutine(CircleChecker());
+		
+		// TransmissionObjectController circleChildren = gameObject.GetComponentsInChildren(typeof(TransmissionObjectController)) as TransmissionObjectController;
+
+		// circleChildren = GetComponentsInChildren<TransmissionObjectController>();
+	}
+	
+	IEnumerator CircleChecker()
+	{
+		while(checkerEnabled != false)
+		{
+			infectedCount = 0;
+			nonInfectedCount = 0;
+			deadCount = 0;
+			recoveredCount = 0;
+			
+			foreach(TransmissionObjectController child in circleChildren)
+			{
+				if(child.IsInfected == true)
+					infectedCount += 1;
+					
+				if(child.IsInfected != true)
+					nonInfectedCount += 1;
+				
+				if(child.IsDead == true)
+					deadCount += 1;
+				
+				if(child.IsRecovered == true)
+					recoveredCount += 1;
+			}
+			
+			Debug.Log("Infected: " + infectedCount);
+			Debug.Log("Not Infected: " + nonInfectedCount);
+			Debug.Log("Dead: " + deadCount);
+			Debug.Log("Recovered: " + recoveredCount);
+			
+			yield return new WaitForSeconds(3f);
+		}
+	}
+}
